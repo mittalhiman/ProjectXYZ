fd5748bbd7fc9c9a56e843eec63d1ad8530aa41d
Mittal Himanshu <mittalhiman@gmail.com>
2018-09-17 12:40:17 +0530

    Made some changes to New Branch
    Made some changes to New Branch

--- first.py
+++ first.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 11 21:49:22 2018
-
 @author: Gupta2 Abhishek
 """
 
@@ -11,9 +10,22 @@ import sys
 
 print(sys.version)
 print("My first python code")
+print("Made some change")
+
 
 #calculating sum of two numbers
 x=2
 y=4
 
 print("Sum of two numbers is {}".format(x+y))
+
+
+#creating pandas DataFrame
+import pandas as pd
+
+data=pd.DataFrame.from_dict(data={'A':{0:1,1:2,2:3},
+                             'B':{0:3,1:4,2:5},
+                             'C':{0:7,1:8,2:9}
+                             })
+
+data.head(1)
