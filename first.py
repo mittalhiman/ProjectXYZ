51656feecf875b1bee63f56f50e86cbc35bc08e4
Mittal Himanshu <mittalhiman@gmail.com>
2018-09-17 11:37:56 +0530

    replaced code file
    Replaced code file with an updated file

--- first.py
+++ first.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 11 21:49:22 2018
-
 @author: Gupta2 Abhishek
 """
 
@@ -17,3 +16,14 @@ x=2
 y=4
 
 print("Sum of two numbers is {}".format(x+y))
+
+
+#creating pandas DataFrame
+import pandas as pd
+
+data=pd.DataFrame.from_dict(data={'A':{0:1,1:2,2:3},
+                             'B':{0:3,1:4,2:5},
+                             'C':{0:7,1:8,2:9}
+                             })
+
+data.head(1)
